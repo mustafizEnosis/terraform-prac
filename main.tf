92f59dc6d69684d167b8c9f759063a23bd6010f6
Mustafizur Rahman <mustafizur.rahman@enosisbd.com>
2025-08-01 20:09:26 +0600

    Add resources of random provider

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ resource "local_file" "games" {
   filename     = "/root/favorite-games"
   content  = "FIFA 21"
 }
+
+resource "random_pet" "other-pet" {
+	      prefix = "Mr"
+	      separator = "."
+	      length = "1"
+}
+
+resource "random_string" "iac_random" {
+  length = 10
+  min_upper = 5
+}
+
