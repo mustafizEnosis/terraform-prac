8cf166416f339ec5bc29a040294e151286079bad
Mustafizur Rahman <mustafizur.rahman@enosisbd.com>
2025-08-04 21:17:29 +0600

    Practice resource types related to AWS

--- aws-infra.tf
+++ aws-infra.tf
@@ -1,7 +1,33 @@
 resource "aws_instance" "dev-server" {
     instance_type = "t2.micro"
     ami         = "ami-02cff456777cd"
+
+    key_name = aws_key_pair.cerberus-key.key_name
+    user_data = file("install-nginx.sh")
 }
 resource "aws_s3_bucket" "falshpoint"  {
     bucket = "project-flashpoint-paradox"
+}
+
+resource "aws_key_pair" "cerberus-key" {
+  key_name = "cerberus"
+  public_key = file("/root/terraform-projects/project-cerberus/.ssh/cerberus.pub")
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+  instance = aws_instance.cerberus.id
+
+  provisioner "local-exec" {
+    command = "echo ${aws_eip.eip.public_dns} >> /root/cerberus_public_dns.txt"
+  }
+}
+
+module "iam_iam-user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "5.28.0"
+  name = "max"
+  create_user = true
+  create_iam_access_key = false
+  create_iam_user_login_profile = false
 }
