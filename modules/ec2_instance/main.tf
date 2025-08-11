487fe6e19ba96d8f1362c394186693d7e9b56b88
Mustafizur Rahman <mustafizur.rahman@enosisbd.com>
2025-08-11 15:32:12 +0600

    Github repo creation

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -18,3 +18,33 @@ resource "aws_instance" "web-2" {
       host = self.public_ip
     }
 }
+
+# Configure the GitHub provider
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# The provider block tells Terraform how to communicate with GitHub.
+# The token is read from the GITHUB_TOKEN environment variable.
+provider "github" {
+  token = "<ACCESS_TOKEN>"
+}
+
+# Define the repository resource
+resource "github_repository" "github_repo" {
+  name        = "terraform-infra"
+  description = "This repository was created using Terraform."
+  visibility  = "public" # Can also be "private"
+  auto_init   = true     # Creates a default README.md file
+}
+
+# Define an output to easily find the repository URL after creation
+output "repository_clone_url_ssh" {
+  value = github_repository.github_repo.ssh_clone_url
+  description = "The URL of the newly created GitHub repository."
+}
